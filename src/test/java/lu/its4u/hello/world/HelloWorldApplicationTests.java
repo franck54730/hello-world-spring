0a8e910a32f91e3e541a7cdace9aaf64f63e83a3
Franck Audiger <franck.audiger@its4u.lu>
2020-05-08 15:17:32 +0200

    change to springboot

---
+++ src/test/java/lu/its4u/hello/world/HelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package lu.its4u.hello.world;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
