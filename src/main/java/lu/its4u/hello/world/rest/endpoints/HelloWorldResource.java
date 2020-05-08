8dfe9be875f810d6831992401b94ca11dd21e104
Franck Audiger <franck.audiger@its4u.lu>
2020-05-08 15:34:15 +0200

    make it works

--- src/main/java/lu/its4u/hello/world/rest/endpoints/HelloWorldResource.java
+++ src/main/java/lu/its4u/hello/world/rest/endpoints/HelloWorldResource.java
@@ -2,28 +2,33 @@ package lu.its4u.hello.world.rest.endpoints;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.ALL_VALUE)
+@EnableAutoConfiguration
 public class HelloWorldResource {
 
 	private static final Logger logger = LoggerFactory.getLogger(HelloWorldResource.class);
 
-	@GetMapping()
+	@GetMapping("/")
 	public String helloWorld() {
 		logger.info("Someone ask a hello from hearth");
 		return "Hello World!";
 	}
 	
 
-	@GetMapping("mars")
+	@GetMapping("/mars")
 	public String helloMars() {
 		logger.info("Someone ask a hello from mars");
 		return "Hello Mars!";
 	}
 
+//	@RequestMapping("/")
+//	public String index() {
+//		return "Greetings from Spring Boot!";
+//	}
 }
